b2c961e1481451da98e91d561e3f2dd65f6214da
Betoside <betoside@gmail.com>
2021-07-15 00:44:03 -0300

    mod12-avancado / 01-mod_reqrite

---
+++ Mod12-PHP-Avancado/01-Mod_rewrite/index.php
@@ -0,0 +1,9 @@
+../< <a href="../">Mod12-PHP-Avancado</a> / <a href="artigo_do_aluno">Reload /artigo_do_aluno</a> /
+<br>
+<br>
+
+<?php
+// echo '<pre>';
+print_r($_GET);
+echo '<br>';
+print_r($_POST);
